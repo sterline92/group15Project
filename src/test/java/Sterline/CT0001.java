2a1402616a3968556ca7fc8d38b2f17ace707679
Sterline Elisee <74082109+sterline92@users.noreply.github.com>
2021-01-29 15:34:11 -0600

    Initial commit

---
+++ src/test/java/Sterline/CT0001.java
@@ -0,0 +1,10 @@
+package Sterline;
+
+public class CT0001 {
+
+    public static void main(String[] args) {
+
+        System.out.println("I can't get it to work");
+        //first initial commit
+    }
+}
